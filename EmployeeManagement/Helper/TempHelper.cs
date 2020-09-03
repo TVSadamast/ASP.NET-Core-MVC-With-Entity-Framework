22231dbadc15039c41924813d8f2495126f76879
Tanmay Sadamast <tanmay@promactInfo.com>
2020-09-04 02:39:54 +0530

    First Commit

---
+++ EmployeeManagement/Helper/TempHelper.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Helper
+{
+    public class TempHelper : DbContext
+    {
+        public TempHelper(DbContextOptions<TempHelper> options) : base(options)
+        {
+        }
+
+        public DbSet<EmployeeModel> EmployeeInfo { get; set; }
+        public DbSet<DepartmentModel> DepartmentInfo { get; set; }
+    }
+}
